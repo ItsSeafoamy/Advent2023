712aaf807102685da06a117b82edb19a3ba8ba7b
Seafoam <laxynd@siblaze.com>
2023-12-14 12:20:19 +0000

    day 14 part 2

--- src/main/java/me/seafoam/advent2023/day14/Main.java
+++ src/main/java/me/seafoam/advent2023/day14/Main.java
@@ -1,11 +1,13 @@
 package me.seafoam.advent2023.day14;
 
+import java.util.HashMap;
 import java.util.List;
 import me.seafoam.advent2023.Puzzle;
 
 public class Main implements Puzzle {
 
-	int[][] map;
+	private int[][] map;
+	private final HashMap<String, Integer> cache = new HashMap<>();
 
 	@Override
 	public void run(List<String> input) {
@@ -16,15 +18,18 @@ public class Main implements Puzzle {
 			default -> throw new IllegalArgumentException("Unexpected value: " + c);
 		}).toArray()).toArray(int[][]::new);
 
-		for (int x = 0; x < map.length; x++) {
-			for (int y = 1; y < map[0].length; y++) {
-				if (y < 1) continue;
-				if (map[y][x] == 1 && map[y - 1][x] == 0) {
-					map[y - 1][x] = 1;
-					map[y][x] = 0;
+		int cycles = 1_000_000_000;
+		for (int i = 0; i < cycles; i++) {
+			tiltNorth();
+			tiltWest();
+			tiltSouth();
+			tiltEast();
 
-					y -= 2;
-				}
+			if (cache.containsKey(mapToString())) {
+				int cycle = i - cache.get(mapToString());
+				while (i + cycle < cycles) i += cycle;
+			} else {
+				cache.put(mapToString(), i);
 			}
 		}
 
@@ -37,5 +42,74 @@ public class Main implements Puzzle {
 		}
 
 		System.out.println(count);
+		// 94590 too high
+	}
+
+	public void tiltNorth() {
+		for (int x = 0; x < map.length; x++) {
+			for (int y = 1; y < map[0].length; y++) {
+				if (y < 1) continue;
+				if (map[y][x] == 1 && map[y - 1][x] == 0) {
+					map[y - 1][x] = 1;
+					map[y][x] = 0;
+
+					y -= 2;
+				}
+			}
+		}
+	}
+
+	public void tiltSouth() {
+		for (int x = 0; x < map.length; x++) {
+			for (int y = map[0].length - 2; y >= 0; y--) {
+				if (y >= map[0].length - 1) continue;
+				if (map[y][x] == 1 && map[y + 1][x] == 0) {
+					map[y + 1][x] = 1;
+					map[y][x] = 0;
+
+					y += 2;
+				}
+			}
+		}
+	}
+
+	public void tiltWest() {
+		for (int y = 0; y < map[0].length; y++) {
+			for (int x = 1; x < map.length; x++) {
+				if (x < 1) continue;
+				if (map[y][x] == 1 && map[y][x - 1] == 0) {
+					map[y][x - 1] = 1;
+					map[y][x] = 0;
+
+					x -= 2;
+				}
+			}
+		}
+	}
+
+	public void tiltEast() {
+		for (int y = 0; y < map[0].length; y++) {
+			for (int x = map.length - 2; x >= 0; x--) {
+				if (x >= map.length - 1) continue;
+				if (map[y][x] == 1 && map[y][x + 1] == 0) {
+					map[y][x + 1] = 1;
+					map[y][x] = 0;
+
+					x += 2;
+				}
+			}
+		}
+	}
+
+	public String mapToString() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int y = 0; y < map[0].length; y++) {
+			for (int x = 0; x < map.length; x++) {
+				sb.append(map[y][x]);
+			}
+		}
+
+		return sb.toString();
 	}
 }
